f45fa26b5f43832f212ad78884d60091f6099dac
YANICK NEDDERHOFF <git@yanicknedderhoff.com>
2015-01-25 16:07:05 +0100

    erste laufende version

---
+++ replies.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# list of random replies
+
+replies_list_wenn = (
+    'Wenn das Woertchen \'wenn\' nicht waer, waer ich heute Millionaer.',
+	'Wenn das mal alles so einfach waer.'
+
+)
+replies_list_haette = (
+    'Haette der Hund nicht geschissen, haette er den Hasen gefangen.',
+	'Haette der Hund nicht geschissen, waere das mit dem Hasen auch anders gelaufen.',
+	'Haette, haette, Fahrradkette.',
+
+)
